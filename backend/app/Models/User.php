dae87998928e2ff6fbb5c15539a5cdf1d0d6941b
Labib Makarim <labibmakarim197@gmail.com>
2025-02-11 22:11:53 +0700

    order model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -30,6 +30,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Cart::class, 'user_id', 'id');
     }
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class, 'user_id', 'id');
+    }
 
     /**
      * The attributes that should be hidden for serialization.
