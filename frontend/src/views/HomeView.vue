9f1ac5d74ea330e3f2bf9a89213f25e56cf3d9fc
Labib Makarim <labibmakarim197@gmail.com>
2025-03-27 21:17:21 +0700

    why choose us section

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeroSection from '@/components/HeroSection.vue'
 import TopBookSection from '@/components/TopBookSection.vue'
+import WhyChooseUsSection from '@/components/WhyChooseUsSection.vue'
 </script>
 
 <template>
   <main>
     <HeroSection />
     <TopBookSection />
+    <WhyChooseUsSection />
   </main>
 </template>
